0ed4ca6ea577ac45a56a6c49d00665f863a2c3ba
Sergey <atomny@rambler.ru>
2020-11-22 22:16:38 +0300

    Добавлен Movie Detail screen

--- app/src/main/java/ru/atomny/aagproject/MovieDetailsActivity.kt
+++ app/src/main/java/ru/atomny/aagproject/MovieDetailsActivity.kt
@@ -3,7 +3,7 @@ package ru.atomny.aagproject
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
